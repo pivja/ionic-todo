3860c7d274d7078c22377c2d3e7a908afd0ac1f8
pivja <nithiphonbuu@hotmail.com>
2016-11-07 19:41:20 +0700

    SuccessLab07

---
+++ ionic-todo/src/providers/data.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class Data {
+
+  constructor(public storage: Storage){
+ 
+  }
+ 
+  getData() {
+    return this.storage.get('todos');  
+  }
+ 
+  save(data){
+    
+    let newData = JSON.stringify(data);
+    this.storage.set('todos', newData);
+  }
+
+}
